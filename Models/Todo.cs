dc0d694c103c1144386018976d91c76006663c8c
Junior gonçalves <decimo.terceiro13@hotmail.com>
2022-12-18 23:15:32 -0300

    0.1

---
+++ Models/Todo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models
+
+{
+
+    public class Todo
+    {
+
+        public int Id { get; set; }
+
+
+        [DisplayName("Titulo")]
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        public string Title { get; set; }
+
+        [DisplayName("Concluido")]
+        public bool Done { get; set; }
+
+        [DisplayName("Criado em ")]
+        public DateTime CreatAt { get; set; } = DateTime.Now;
+
+        [DisplayName("Ultima Atualização")]
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+
+        [DisplayName("Usuario")]
+        public string User { get; set; }
+
+
+
+    }
+}
